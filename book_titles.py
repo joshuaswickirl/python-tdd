2f2a203d89337af316ab621926f84978c6a7eb5a
Joshua Swick <joshuaswickirl@gmail.com>
2021-07-23 22:19:10 -0600

    Add solution

--- book_titles.py
+++ book_titles.py
@@ -1,3 +1,13 @@
 def calculate(book_name):
     result = book_name[0] + str(len(book_name))
     return result
+
+file = open("books.txt", "r")
+
+lines = file.readlines()
+for line in lines:
+    book_name = line[0:len(line)-1]
+    result = calculate(book_name)
+    print(result)
+
+file.close()
