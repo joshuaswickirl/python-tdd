c783c6aee826d4e561e1d12fd19ff599c932765f
Joshua Swick <joshuaswickirl@gmail.com>
2021-07-23 22:29:48 -0600

    Fix file parsing bug

--- book_titles.py
+++ book_titles.py
@@ -2,11 +2,12 @@ def calculate(book_name):
     result = book_name[0] + str(len(book_name))
     return result
 
-file = open("books.txt", "r")
+def parseFile(file):
+    return file.read().splitlines()
 
-lines = file.readlines()
-for line in lines:
-    book_name = line[0:len(line)-1]
+file = open("books.txt", "r")
+books_list = parseFile(file)
+for book_name in books_list:
     result = calculate(book_name)
     print(result)
 
